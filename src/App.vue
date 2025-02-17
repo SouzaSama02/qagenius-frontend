7cd52cbe351acfff763d4983524d5867e6af6c55
Souza <bossviti064@gmail.com>
2025-02-17 10:31:03 -0300

    :sparkles: feat: toggle-task-feedback

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,15 @@ import { ref } from "vue";
 import inputqa from "./components/inputqa.vue";
 import outputqa from "./components/outputqa.vue";
 import introduce from "./components/introduce.vue";
+import togglebutton from "./components/togglebutton.vue";
 
 const outputMessage = ref("");
 const loading = ref(false);
+const onToggleFileType = ref(false);
+
+function setFileType(value) {
+  onToggleFileType.value = value;
+}
 
 function setLoading(value) {
   loading.value = value;
@@ -19,14 +25,23 @@ function setResponseData(value) {
 
 <template>
   <div class="mockup-browser bg-base-300 border">
-    <div class="mockup-browser-toolbar"></div>
+    <div class="mockup-browser-toolbar">
+      <div class="ml-auto">
+        <togglebutton @toggleFileType="setFileType" />
+      </div>
+    </div>
     <div class="bg-white justify-center px-4 py-8">
       <introduce class="mb-12" />
       <div
         class="flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-8"
       >
         <div class="w-full md:w-auto">
-          <inputqa @sendOutput="setResponseData" @loading="setLoading" />
+          // props deve passar a variavel fileType para o componente inputqa
+          <inputqa
+            :fileType="onToggleFileType"
+            @sendOutput="setResponseData"
+            @loading="setLoading"
+          />
         </div>
 
         <div class="w-full md:w-auto">
---
+++ src/components/togglebutton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["toggleFileType"]);
+const fileType = ref(false);
+
+function toggleFileType() {
+  fileType.value = !fileType.value;
+  emit("toggleFileType", fileType.value);
+}
+</script>
+
+<template>
+  <div class="ml-auto">
+    <label class="label cursor-pointer">
+      <span class="label-text">Feedback</span>
+      <input type="checkbox" @click="toggleFileType" class="toggle mx-9" />
+      <span class="label-text">Tarefa</span>
+    </label>
+  </div>
+</template>
